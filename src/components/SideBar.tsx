101fcbdb3cacb8973a35b0f6b318cca62babe6e4
acferM <matheus.acfer@nave.org.br>
2021-03-15 12:13:05 -0300

    code separated in components

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,40 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button'
+
+import '../styles/sidebar.scss';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SidebarProps {
+  genres: GenreResponseProps[]
+  changeSelectedGenreIdFunction: React.Dispatch<React.SetStateAction<number>>
+  selectedGenreId: number
+}
+
+export function SideBar({ genres, changeSelectedGenreIdFunction, selectedGenreId }: SidebarProps) {
+  function handleClickButton(id: number) {
+    changeSelectedGenreIdFunction(id);
+  }
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            id={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
